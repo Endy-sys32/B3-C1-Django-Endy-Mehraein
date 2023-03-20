3f87f8b77f3aa490a1cb3c8920512d2f73222673
Endy Mehraein <endymehraein@EM1.local>
2023-03-20 03:13:39 +0100

    ajout model

--- pilotage/src/admin.py
+++ pilotage/src/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TypePilotage
+from .models import VehiculePilotage
+from .models import CoursPilotage
+from .models import Reservation
+from .models import User
 
-# Register your models here.
+admin.site.register(TypePilotage)
+admin.site.register(VehiculePilotage)
+admin.site.register(CoursPilotage)
+admin.site.register(Reservation)
+admin.site.register(User)
--- pilotage/src/models.py
+++ pilotage/src/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class TypePilotage(models.Model):
+    id_type = models.IntegerField(primary_key=True)
+    libelle_pilotage = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.libelle_pilotage
+
+class VehiculePilotage(models.Model):
+    id_vehicule = models.IntegerField(primary_key=True)
+    marque_vehicule = models.CharField(max_length=200)
+    model_vehicule = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.model_vehicule
+
+class CoursPilotage(models.Model):
+    id_cours = models.IntegerField(primary_key=True)
+    id_type = models.ForeignKey(TypePilotage, on_delete=models.CASCADE)
+    id_vehicule = models.ForeignKey(VehiculePilotage, on_delete=models.CASCADE)
+
+class User(models.Model):
+    id_user = models.IntegerField(primary_key=True)
+    nom_user = models.CharField(max_length=200)
+    prenom_user = models.CharField(max_length=300)
+    login_user = models.CharField(max_length=400)
+    password_user = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nom_user
+
+class Reservation(models.Model):
+    id_reservation = models.IntegerField(primary_key=True)
+    cours = models.ForeignKey(CoursPilotage, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_reservation = models.DateField()
+    heure_deb_reservation = models.DateTimeField()
+    heure_fin_reservation = models.DateTimeField()
+
+    def __str__(self):
+        return self.user
