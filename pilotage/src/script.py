6b593e7518a5bd3e7db2aed3920d861ccdc032a1
Endy Mehraein <endymehraein@em1.home>
2023-04-10 11:46:22 +0200

    add multiplication and divison test

--- pilotage/src/script.py
+++ pilotage/src/script.py
@@ -2,3 +2,7 @@ def add(x,y):
     return x+y
 def soustraction(x,y):
     return x-y
+def multiplication(x,y):
+    return x*y
+def division(x,y):
+    return x/y
--- pilotage/src/tests.py
+++ pilotage/src/tests.py
@@ -20,3 +20,22 @@ class Test(TestCase):
         self.assertEqual(soustraction(3,2), 1)
     def test_soustract4(self):
         self.assertEqual(soustraction(3, 3), 0)
+
+    def test_multiplication1(self):
+        self.assertEqual(multiplication(2, 3), 6)
+    def test_multiplication2(self):
+        self.assertEqual(multiplication(2, 2), 4)
+    def test_multiplication3(self):
+        self.assertEqual(multiplication(3,2), 6)
+    def test_multiplication4(self):
+        self.assertEqual(multiplication(3, 3), 9)
+
+    def test_division1(self):
+        self.assertEqual(division(2, 3), 0.6666666666666666)
+    def test_division2(self):
+        self.assertEqual(division(2, 2), 1)
+    def test_division3(self):
+        self.assertEqual(division(3,2), 1.5)
+    def test_division4(self):
+        self.assertEqual(division(3, 3), 1)
+
